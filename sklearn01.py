e124326a73b656e3be7a87c103011afd58f8638e
junhao-tan <junhao.t@gmail.com>
2018-09-01 22:59:52 +0800

    correct syntax

--- sklearn01.py
+++ sklearn01.py
@@ -9,7 +9,7 @@ import sklearn
 #load the data
 oecd_bli = pd.read_csv("oecd_bli_2015.csv", thousands=',')
 gdp_per_capita = pd.read_csv("gdp_per_capita.csv", thousands=',', delimiter='\t',
-                            encoding='latin1'. na_values="n/a")
+                            encoding='latin1', na_values="n/a")
 
 #Prepare the data
 country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
